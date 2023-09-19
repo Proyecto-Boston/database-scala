cd0fda1f29c185ff9e8d9783f2a3eb4df808e8ef
Jean-Parra <jean.parra.2020@upb.edu.co>
2023-09-18 22:42:49 -0500

    Database - test

--- database/init.sql
+++ database/init.sql
@@ -1,52 +1,79 @@
-CREATE TABLE `usuarios` (
-	`id` INT(11) NOT NULL AUTO_INCREMENT,
-	`nombre` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_general_ci',
-	`apellido` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_general_ci',
-	`habilitado` TINYINT(1) NOT NULL DEFAULT '1',
-	PRIMARY KEY (`id`) USING BTREE
-)
-COLLATE='utf8mb4_general_ci'
-ENGINE=InnoDB
-; 
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Versión del servidor:         11.2.0-MariaDB - mariadb.org binary distribution
+-- SO del servidor:              Win64
+-- HeidiSQL Versión:             12.3.0.6589
+-- --------------------------------------------------------
 
-CREATE TABLE `directorios` (
-	`id` INT(11) NOT NULL AUTO_INCREMENT,
-	`nombre` VARCHAR(100) NOT NULL COLLATE 'utf8mb4_general_ci',
-	`ruta` VARCHAR(200) NOT NULL COLLATE 'utf8mb4_general_ci',
-	`usuario_id` INT(11) NOT NULL,
-	PRIMARY KEY (`id`) USING BTREE,
-	INDEX `usuario_id` (`usuario_id`) USING BTREE,
-	CONSTRAINT `FK__usuarios_directorios` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`) ON UPDATE NO ACTION ON DELETE NO ACTION
-)
-COLLATE='utf8mb4_general_ci'
-ENGINE=InnoDB
-;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE TABLE `archivos` (
-	`id` INT(11) NOT NULL AUTO_INCREMENT,
-	`nombre` VARCHAR(100) NOT NULL COLLATE 'utf8mb4_general_ci',
-	`ruta` VARCHAR(200) NOT NULL COLLATE 'utf8mb4_general_ci',
-	`tamano` DOUBLE NOT NULL,
-	`usuario_id` INT(11) NOT NULL,
-	`habilitado` TINYINT(1) NOT NULL DEFAULT '1',
-	PRIMARY KEY (`id`) USING BTREE,
-	INDEX `usuario_id` (`usuario_id`) USING BTREE,
-	CONSTRAINT `FK__usuarios_archivos` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`) ON UPDATE RESTRICT ON DELETE RESTRICT
-)
-COLLATE='utf8mb4_general_ci'
-ENGINE=InnoDB
-;
 
-CREATE TABLE `compartidos` (
-	`id` INT(11) NOT NULL AUTO_INCREMENT,
-	`archivo_id` INT(11) NOT NULL,
-	`usuario_id` INT(11) NOT NULL,
-	PRIMARY KEY (`id`) USING BTREE,
-	INDEX `archivo_id` (`archivo_id`) USING BTREE,
-	INDEX `usuario_id` (`usuario_id`) USING BTREE,
-	CONSTRAINT `FK__archivo_compartidos` FOREIGN KEY (`archivo_id`) REFERENCES `archivos` (`id`) ON UPDATE RESTRICT ON DELETE RESTRICT,
-	CONSTRAINT `FK__usuarios_compartidos` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`) ON UPDATE RESTRICT ON DELETE RESTRICT
-)
-COLLATE='utf8mb4_general_ci'
-ENGINE=InnoDB
-;
+-- Volcando estructura de base de datos para proyecto_aula
+CREATE DATABASE IF NOT EXISTS `proyecto_aula` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+USE `proyecto_aula`;
+
+-- Volcando estructura para tabla proyecto_aula.archivos
+CREATE TABLE IF NOT EXISTS `archivos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) NOT NULL,
+  `ruta` varchar(200) NOT NULL,
+  `tamano` double NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  `habilitado` tinyint(1) NOT NULL DEFAULT 1,
+  PRIMARY KEY (`id`) USING BTREE,
+  KEY `usuario_id` (`usuario_id`) USING BTREE,
+  CONSTRAINT `FK__usuarios_archivos` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- La exportación de datos fue deseleccionada.
+
+-- Volcando estructura para tabla proyecto_aula.compartidos
+CREATE TABLE IF NOT EXISTS `compartidos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `archivo_id` int(11) NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  KEY `archivo_id` (`archivo_id`) USING BTREE,
+  KEY `usuario_id` (`usuario_id`) USING BTREE,
+  CONSTRAINT `FK__archivo_compartidos` FOREIGN KEY (`archivo_id`) REFERENCES `archivos` (`id`),
+  CONSTRAINT `FK__usuarios_compartidos` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- La exportación de datos fue deseleccionada.
+
+-- Volcando estructura para tabla proyecto_aula.directorios
+CREATE TABLE IF NOT EXISTS `directorios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) NOT NULL,
+  `ruta` varchar(200) NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `usuario_id` (`usuario_id`),
+  CONSTRAINT `FK__usuarios_directorios` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- La exportación de datos fue deseleccionada.
+
+-- Volcando estructura para tabla proyecto_aula.usuarios
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) NOT NULL,
+  `apellido` varchar(50) NOT NULL,
+  `habilitado` tinyint(1) NOT NULL DEFAULT 1,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- La exportación de datos fue deseleccionada.
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
