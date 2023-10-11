ef7f31372bba34d4d444d2d1beeb0e9fd47e5d74
QuesitoCode <julianfe00@gmail.com>
2023-10-10 22:54:21 -0500

    Correcciones SharedRoute

--- src/main/scala/example/routes/SharedRoute.scala
+++ src/main/scala/example/routes/SharedRoute.scala
@@ -6,7 +6,7 @@ import controllers.SharedController
 import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport._
 import io.circe.generic.auto._
 import akka.http.scaladsl.model.{HttpResponse, StatusCodes}
-import models.{sharedModel, sharedCreateModel}
+import models.{SharedModel, SharedCreateModel}
 import scala.concurrent.Future
 import com.typesafe.config.ConfigFactory
 
@@ -15,7 +15,7 @@ class SharedRoute(SharedController: SharedController) {
   val route: Route = pathPrefix("shared") {
     get {
       path(IntNumber) { id =>
-        val result: Future[Either[String, sharedModel]] = SharedController.buscarUsuario(id)
+        val result: Future[Either[String, SharedModel]] = SharedController.obtenerCompartidosPorUsuario(id)
         onSuccess(result) {
           case Right(shared)      => complete(shared)
           case Left(errorMessage) => complete(HttpResponse(StatusCodes.NotFound, entity = errorMessage))
@@ -24,8 +24,8 @@ class SharedRoute(SharedController: SharedController) {
     } ~
       path("register") {
         post {
-          entity(as[sharedCreateModel]) { shared =>
-            val result: Future[Either[String, sharedModel]] =
+          entity(as[SharedCreateModel]) { shared =>
+            val result: Future[Either[String, SharedModel]] =
               SharedController.guardarCompartido(shared.usuario_id, shared.archivo_id)
             onSuccess(result) {
               case Right(newshared)   => complete(StatusCodes.Created, newshared)
@@ -36,8 +36,8 @@ class SharedRoute(SharedController: SharedController) {
       } ~
       path("delete") {
         put {
-          entity(as[sharedCreateModel]) { id =>
-            val result: Future[Either[String, sharedModel]] = SharedController.eliminarCompartido(id)
+          entity(as[SharedModel]) { id =>
+            val result: Future[Either[String, SharedModel]] = SharedController.eliminarCompartido(id)
             onSuccess(result) {
               case Right(newshared)   => complete(StatusCodes.Created, newshared)
               case Left(errorMessage) => complete(HttpResponse(StatusCodes.InternalServerError, entity = errorMessage))
